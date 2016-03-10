8a48a94099baa3acfe32ab4285b9c5afcf3c9080
Medhyena <link684@msn.com>
2016-03-10 19:24:38 +0100

    rajout classeligne

---
+++ classeligne.h
@@ -0,0 +1,12 @@
+#ifndef "classeligne_h"
+#define "classeligne_h"
+
+class ligne {
+	// Private section
+	public:
+		// Public Declarations
+		ligne();
+	protected:
+		// Protected Declarations
+};
+
