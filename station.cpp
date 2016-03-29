98c13bb77693e76da071044e1b40a71bd571a67d
Medhyena <link684@msn.com>
2016-03-29 13:29:54 +0200

    norme euclidienne distance

--- station.cpp
+++ station.cpp
@@ -1,4 +1,10 @@
 #include "station.h"
 
-Station::Station()
+Station::Station():posX(0),posY(0)
 {}
+
+double Station::distance(const Station &S2) const
+{
+  int X=S2.posX-posX,Y=S2.posY-posY;
+  return sqrt((X*X)+(Y*Y));
+}
