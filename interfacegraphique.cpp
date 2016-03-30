3068384c9c543bb29cfbe9332dd61543db9093b3
Songfu <simon.gourdeau@hotmail.fr>
2016-03-30 11:26:14 +0200

    Lire description #passérieux
    J'ai juste récup les fonctions utiles pour pouvoir faire l'interface
    graphique.
    Le fichier .dev est bien modif à l'ajout d'un fichier (normal quoi)

---
+++ interfacegraphique.cpp
@@ -0,0 +1,11 @@
+#include "graphics.h"
+
+//Fonctions à utiliser pour l'interface graphique
+
+//void line(int x0, int y0, int x1, int y1)
+
+//setcolor(int c) (ex c=RED)
+
+//void circle(int x, int y, int radius)
+
+//void rectangle (int left, int top, int right, int bottom);
